fdea2b3c51265f478a580e78edf338ba0f6bd92b
Felix Abrahamsson <felix@aiwizo.com>
2019-10-17 10:08:28 +0200

    Fix device issue

--- mixmatch/guess_targets.py
+++ mixmatch/guess_targets.py
@@ -5,13 +5,15 @@ from .tile_adjacent import tile_adjacent
 
 def guess_targets(features, model, output_transform, K, T):
 
+    features_device = features.device
     with torch.no_grad():
+        features = features.to(next(model.parameters()).device)
         probabilities = output_transform(model(features))
         probabilities = (
             probabilities
             .view(-1, K, *probabilities.shape[1:])
             .mean(dim=1)
         )
-        probabilities = sharpen(probabilities, T)
+        probabilities = sharpen(probabilities, T).to(features_device)
 
     return tile_adjacent(probabilities, K)
